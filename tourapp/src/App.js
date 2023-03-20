475c87dc8ec1a3e537795b5167b8aa5bd1ca2ccb
shnorlaxzz <mshnas56@gmail.com>
2023-03-20 08:25:57 +0000

    lab17 done

--- tourapp/src/App.js
+++ tourapp/src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
 import Home from './components/home/Home';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Routes, Route} from 'react-router-dom';// from documentation
+import Tour from './components/tours/tour/Tour';
+import TourDetails from './components/TourDetails/TourDetails';
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
 function App() {
+  const data = require ("./data/data.json")
   return (
     <>
-      <Home/>
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/city/:id" element={<TourDetails arrData={data}/>}></Route>
+      </Routes>
+      <Footer/>
     </>
   );
 }
--- tourapp/src/components/home/Home.js
+++ tourapp/src/components/home/Home.js
@@ -1,15 +1,13 @@
+// Home.js
 import Tours from "../tours/Tours";
-import Header from "../header/Header";
-import Footer from "../footer/Footer";
+// import Header from "../header/Header";
+// import Footer from "../footer/Footer";
 // import data from "../../data/data.json"
 const data = require ("../../data/data.json")
 function Home(){
     return(
         <div>
-            <Header/>
-            <h2>Home Page</h2>
             <Tours arrData={data}/>
-            <Footer/>
         </div>
         
     )
